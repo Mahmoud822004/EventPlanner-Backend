815543a5e0f37d4da6d86ed102dd2835d1f3cf87
Khaled301204 <khaledharfoosh2014@gmail.com>
2025-12-26 23:15:38 +0200

    last update insha'allah

--- main.go
+++ main.go
@@ -15,20 +15,16 @@ func main() {
 
 	database.DB.AutoMigrate(&models.User{}, &models.Event{}, &models.Invitation{})
 
-		r := gin.Default()
+	r := gin.Default()
 
-		r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://eventplanner-frontend-khaledahmed04-dev.apps.rm2.thpm.p1.openshiftapps.com"},
 		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
-
-	
-
-
 
 	routes.AuthRoutes(r)
 	routes.RegisterRoutes(r)
